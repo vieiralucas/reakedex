cf9b2d78386c78a0ef06db8f90b5344dfb007cc4
Lucas Vieira <vieiralucas4@gmail.com>
2016-07-04 00:10:08 -0300

    Deploy yo gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,54 @@
-import path from 'path';
-import webpack from 'webpack';
+const path = require('path');
+const webpack = require('webpack');
 
-export default {
-  devtool: 'source-map',
-  entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    './src/index'
-  ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  module: {
-    loaders: [
-      {test: /\.js$/, loaders: ['react-hot', 'babel'], include: path.join(__dirname, 'src')},
-      {test: /\.less$/, loader: 'style!css!less'}
-    ]
-  }
-};
+const deploy = process.env.NODE_ENV === 'production' || false;
+let config = {};
+
+if (deploy) {
+  config = {
+    devtool: 'source-map',
+    entry: [
+      './src/index'
+    ],
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: 'bundle.min.js',
+    },
+    plugins: [
+			new webpack.optimize.DedupePlugin(),
+			new webpack.optimize.UglifyJsPlugin(),
+			new webpack.optimize.AggressiveMergingPlugin()
+    ],
+    module: {
+      loaders: [
+        {test: /\.js$/, loaders: ['babel'], include: path.join(__dirname, 'src')},
+        {test: /\.less$/, loader: 'style!css!less'}
+      ]
+    }
+  };
+} else {
+  config = {
+    devtool: 'source-map',
+    entry: [
+      'webpack-dev-server/client?http://localhost:3000',
+      'webpack/hot/only-dev-server',
+      './src/index'
+    ],
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: 'bundle.js',
+      publicPath: '/static/'
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ],
+    module: {
+      loaders: [
+        {test: /\.js$/, loaders: ['react-hot', 'babel'], include: path.join(__dirname, 'src')},
+        {test: /\.less$/, loader: 'style!css!less'}
+      ]
+    }
+  };
+}
+
+module.exports = config;
