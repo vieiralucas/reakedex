29f2fe273f4103933ba2886341b38e8157ce293f
Lucas Vieira <vieiralucas4@gmail.com>
2016-07-07 19:45:05 -0300

    Remove components tests for now

--- test/components/PokemonItem.test.js
+++ test/components/PokemonItem.test.js
@@ -3,7 +3,4 @@ import React from 'react';
 import PokemonItem from '../../src/components/PokemonItem';
 
 describe('components/PokemonItem.js', function() {
-  it('should render a pokemon name inside a li', function() {
-    expect(1).to.eql(2);
-  });
 });
--- test/components/PokemonList.test.js
+++ test/components/PokemonList.test.js
@@ -4,7 +4,4 @@ import PokemonList from '../../src/components/PokemonList';
 import PokemonItem from '../../src/components/PokemonItem';
 
 describe('components/PokemonList.js', function() {
-  it('should render a ul of PokemonList', function() {
-    expect(1).to.eql(2);
-  });
 });
